3014c50fd4073b4bdcc7106f9012f6aa4d69e3c7
uzerjamal <uzerjamal@gmail.com>
2017-03-06 22:29:22 +0530

    V0.3
    Can select pieces now using a raycast
    selected piece grows in size to indicate selection

---
+++ Anti Chess/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+    private RaycastHit hit;
+
+    private Vector3 scaleUpOnSelect = new Vector3(1.25f, 1.25f, 1);
+    private Vector3 scaleBackDownOnDeselect = new Vector3(1, 1, 1);
+
+    private GameObject selectedPiece;
+
+    private bool isWhiteTurn = true;
+
+    private int numberOfPiecesSelected = 0;
+
+
+
+    void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 20.0f, LayerMask.GetMask("Pieces")))
+            {
+                if (numberOfPiecesSelected >= 1)
+                {
+                    selectedPiece.transform.localScale = scaleBackDownOnDeselect;
+                    selectedPiece = default(GameObject);
+                    numberOfPiecesSelected--;
+                }
+                    selectedPiece = hit.transform.gameObject;
+                    selectedPiece.transform.localScale = scaleUpOnSelect;
+                    numberOfPiecesSelected++;
+            }
+
+
+        }
+        
+
+    }
+
+
+}
